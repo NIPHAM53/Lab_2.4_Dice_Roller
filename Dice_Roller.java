3528e2f5eb91b2f7cf7ad441600d2d4697e52508
NIPHAM53 <nipham53@gmail.com>
2022-04-06 00:48:01 -0400

    Added ability to validate users input of Y or N to continue loop

--- Dice_Roller.java
+++ Dice_Roller.java
@@ -6,17 +6,30 @@ public class Dice_Roller {
 		System.out.println("Welcome to the Grand Circus Casino!");
 		Scanner scnr = new Scanner(System.in);
 		String choice = "y";
+		boolean valid = false;
 		do {
 			System.out.println("How many sides should each die have? ");
 			int numSides = scnr.nextInt();
-			int d1 = (int) (numSides * Math.random());
-			int d2 = (int) (numSides * Math.random());
-			System.out.println("Roll 1:" + "\n" + d1 + "\n" + d2);
-			System.out.println("Roll again? (y/n)");
-			choice = scnr.next();
+			int d1 = (int) (numSides * Math.random()) + 1;
+			int d2 = (int) (numSides * Math.random()) + 1;
+			System.out.println("Roll result:" + "\n" + d1 + "\n" + d2);
+			while (!valid) {
+				System.out.println("Roll again? (y/n)");
+				choice = scnr.next();
+				if (choice.contains("y")) {
+					valid = true;
+					System.out.println("Next Round!");
+				} else if (choice.contains("n")) {
+					valid = true;
+					System.out.println("Thank you for playing! See you next time");
+
+				} else {
+					System.out.println("Invalid input. Please type in y or n");
+				}
+
+			}
+
 		} while (choice.equalsIgnoreCase("y"));
-		System.out.println("Thank you for playing! Please come again.");
-
+		System.out.println("Thank you for playing! See you next time");
 	}
-
 }
